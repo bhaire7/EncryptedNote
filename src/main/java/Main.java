2fea2f83898a4ab9318a46ee57e618c88fedd3ef
bhaire7 <baibhavnepal07@gmail.com>
2026-01-30 22:23:52 +0545

    v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import ui.LogInRegisterUI.Mainframe;
 public class Main {
     public static void main(String[] args) {
         AdminDB.initializeAdmin();
+        //v1
         new Mainframe();
     }
 }
